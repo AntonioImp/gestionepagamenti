a793d7dab662eeb92535b7e7028b87ad8a308b74
AntonioImp <48328650+AntonioImp@users.noreply.github.com>
2020-12-26 16:01:15 +0100

    Initial commit

---
+++ src/main/java/dsbd2020/ecommerce/gestionepagamenti/GestionepagamentiApplication.java
@@ -0,0 +1,13 @@
+package dsbd2020.ecommerce.gestionepagamenti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionepagamentiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionepagamentiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dsbd2020/ecommerce/gestionepagamenti/GestionepagamentiApplicationTests.java
@@ -0,0 +1,13 @@
+package dsbd2020.ecommerce.gestionepagamenti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionepagamentiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
